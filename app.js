4111ee86e48c5b7f66843efe7e588f2e6b2fcd72
Stefan <info@cruftcam.com>
2019-02-19 13:20:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Steuert eine einfache Masterpage und die zugehörigen Unterseiten.
+
 
 var express = require('express'),    
     app = express(),
@@ -8,14 +8,13 @@ var express = require('express'),
 app.use('/', app_router);
 
 
-app.engine('html', engines.nunjucks);   // Einbinden bzw. verwenden der Templates aus dem Ordner View
+app.engine('html', engines.nunjucks);   
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.use(express.static('views')); // HURRA !? Dies ist die Lösung zum einbinden von statischen Dateien wie CSS oder Image. Das kommt ins Tutorial. 
-// Kann man diese statischen Dateien zur Laufzeit bearbeiten bzw. manipulieren ??? Wäre ein nettes Beispiel.
+app.use(express.static('views')); 
 
-// Server starten.
+// start server
 var server = app.listen(3000, function() {
     var port = server.address().port;
     console.log('Express server listening on port %s', port);
